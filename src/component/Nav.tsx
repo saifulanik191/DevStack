89fd077d70de5c3cff67c4635c673966f862626e
saifulanik191 <anik8187@gmail.com>
2026-09-13 21:43:07 +0600

    Change some tailwind css to fix type relaated issue

--- src/component/Nav.tsx
+++ src/component/Nav.tsx
@@ -5,12 +5,12 @@ const Nav = () => {
     <nav className="border border-[#05050521]">
       <div className="container mx-auto flex items-center justify-between py-4">
         <img
-          className="order-2 h-14 md:order-none md:block md:h-auto"
+          className="order-2 h-14 md:order-0 md:block md:h-14"
           src={Logo}
           alt="Logo"
         />
 
-        <ul className="hidden space-x-4 text-md md:flex md:order-none">
+        <ul className="hidden space-x-4 text-md md:flex md:order-0">
           <li>Home</li>
           <li>Technologies</li>
           <li>Projects</li>
@@ -18,14 +18,14 @@ const Nav = () => {
           <li>Contact</li>
         </ul>
 
-        <div className="order-3 flex gap-2 md:order-none md:flex md:gap-4">
+        <div className="order-3 flex gap-2 md:order-0 md:flex md:gap-4">
           <button>Sign In</button>
           <button className="btn btn-secondary rounded-4xl bg-linear-to-r from-orange-500 via-pink-500">
             Sign Up
           </button>
         </div>
 
-        <button className="order-1 text-4xl text-gray-500 md:order-none md:hidden">
+        <button className="order-1 text-4xl text-gray-500 md:order-0 md:hidden">
           <img src={HamBerger} alt="" />
         </button>
       </div>
